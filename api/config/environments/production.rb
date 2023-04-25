51b6c87659fb510b8e204c2be7c8ad7266de2c17
Brodey Newman <brodeynewman@gmail.com>
2023-04-25 19:48:49 -0400

    chore: rm redis config

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -85,10 +85,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   config.cache_store = :redis_cache_store, {
     url: ENV['REDIS_URL'],
-    connect_timeout:    30,  # Defaults to 20 seconds
-    read_timeout:       0.2, # Defaults to 1 second
-    write_timeout:      0.2, # Defaults to 1 second
-    reconnect_attempts: 1,   # Defaults to 0
 
     error_handler: -> (method:, returning:, exception:) {
       Rails.logger.error "Redis error: #{exception}"
